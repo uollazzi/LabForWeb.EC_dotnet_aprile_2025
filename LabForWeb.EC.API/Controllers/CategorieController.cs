1e02ae45b7fc2602e95be83cec6a8cb6e92d99b3
Matteo Valenzi <dilloawallace@hotmail.com>
2025-07-09 12:35:24 +0200

    .

--- LabForWeb.EC.API/Controllers/CategorieController.cs
+++ LabForWeb.EC.API/Controllers/CategorieController.cs
@@ -1,6 +1,7 @@
 ﻿using LabForWeb.EC.API.Extensions;
 using LabForWeb.EC.API.Models;
 using LabForWeb.EC.DAL;
+using LabForWeb.EC.DAL.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,12 +18,109 @@ namespace LabForWeb.EC.API.Controllers
             _dc = dc;
         }
 
-        [HttpGet]
+        [HttpGet] // GET /api/categorie
         public async Task<ActionResult<IEnumerable<CategoriaModel>>> GetAll()
         {
             var data = await _dc.Categorie.Select(c => c.ToCategoriaModel()).ToListAsync();
             return Ok(data);
         }
 
+        [HttpGet("{id}")] // GET /api/categorie/76
+        public async Task<ActionResult<CategoriaModel>> GetById(int id)
+        {
+            var cat = await _dc.Categorie.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cat.ToCategoriaModel());
+        }
+
+        [HttpPost] // POST /api/categorie
+        public async Task<ActionResult<CategoriaModel>> Add(CategoriaDTO item)
+        {
+            // oggetto da salvare sul DB
+            var cat = new Categoria
+            {
+                Nome = item.Nome
+            };
+
+            try
+            {
+                _dc.Categorie.Add(cat);
+                await _dc.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest(ex.InnerException?.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+
+            return Ok(cat.ToCategoriaModel());
+        }
+
+
+        //public async Task<ActionResult<CategoriaModel>> Update([FromRoute] int id, [FromBody] CategoriaDTO item)
+
+        [HttpPut("{id}")] // PUT /api/categorie/3
+        public async Task<ActionResult<CategoriaModel>> Update(int id, CategoriaDTO item)
+        {
+            // oggetto da salvare sul DB
+            //var cat = await _dc.Categorie.SingleOrDefaultAsync(x => x.Id == id); // alternativa
+            var cat = await _dc.Categorie.FindAsync(id);
+
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                cat.Nome = item.Nome;
+                await _dc.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest(ex.InnerException?.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+
+            return Ok(cat.ToCategoriaModel());
+        }
+
+        [HttpDelete("{id}")] // DELETE /api/categorie/3
+        public async Task<ActionResult> Delete(int id)
+        {            
+            var cat = await _dc.Categorie.FindAsync(id);
+
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _dc.Categorie.Remove(cat);
+                await _dc.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest(ex.InnerException?.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+
+            return NoContent();
+        }
     }
 }
---
+++ LabForWeb.EC.API/Models/CategoriaDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabForWeb.EC.API.Models;
+
+// DTO => Data Transfer Object
+public class CategoriaDTO
+{
+    [Required(ErrorMessage ="Il campo {0} è obbligatorio")]
+    public string? Nome{ get; set; }
+}
